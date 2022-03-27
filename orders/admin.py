ec63f965c2ac9c9bffaa159d42c46dde6a18beea
odinSanliveira <odin.sanliveira@gmail.com>
2022-03-27 14:23:03 -0300

    setting orders model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Order
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAAdmin(admin.ModelAdmin):
+    list_display=['id','size', 'order_status', 'quantity', 'created_at', 'customer']
+    list_filter=['created_at', 'order_status', 'size', 'customer']
+
+
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+#Orders model
+class Order(models.Model):
+    SIZES=(
+        ('SMALL', 'small'),
+        ('MEDIUM', 'medium'),
+        ('LARGE', 'large'),
+        ('EXTRA_LARGE', 'extraLarge'),
+    )
+    ORDER_STATUS=(
+        ('PENDING', 'pending'),
+        ('IN_TRANSIT', 'inTransit'),
+        ('DELIVERED', 'delivered'),
+        )
+        
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.CharField(max_length=20, choices=SIZES,default=SIZES[0][0])
+    order_status = models.CharField(max_length=20, choices=ORDER_STATUS, default=ORDER_STATUS[0][0])
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at=models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'<Order {self.size} by {self.customer.username}'
