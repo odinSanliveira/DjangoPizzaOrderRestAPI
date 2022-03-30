0436e5a36f8b575e019472f2b0eb4e2bed23dab2
odinSanliveira <odin.sanliveira@gmail.com>
2022-03-30 13:43:50 -0300

    fixing settings configurations.

--- pizza/settings.py
+++ pizza/settings.py
@@ -11,8 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', cast=bool)
+# SECRET_KEY = config('SECRET_KEY')
+# DEBUG = config('DEBUG', cast=bool)
+
+SECRET_KEY = 'e27c923e758a96b54ee658f1'
+DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
